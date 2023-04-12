a49490eb1cd2281bd6afad08272477bf607ea164
Deepesh Pandey <100483463+Deepesh-93@users.noreply.github.com>
2023-04-12 14:48:31 +0530

    Add files via upload

---
+++ fooditem.js
@@ -0,0 +1,404 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Ambur Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 13,
+    img: 'https://static.toiimg.com/thumb/54308405.cms?width=1200&height=900',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Hyderabadi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'https://recipes.timesofindia.com/thumb/msid-53096628,width-1600,height-900/53096628.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Egg Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 18,
+    img: 'https://www.pavaniskitchen.com/wp-content/uploads/2021/04/egg-biryani-recipe.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Goan Fish Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'https://previews.123rf.com/images/vm2002/vm20021603/vm2002160300014/54725942-fish-biryani-indian-style-fish-and-rice-with-spicy-masala-and-color.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Mutton Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 10,
+    img: 'https://www.spiceeats.com/wp-content/uploads/2020/07/Mutton-Biryani.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Kamrupi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 12,
+    img: 'https://www.youngisthan.in/wp-content/uploads/2018/08/kol.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Kashmiri Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 13,
+    img: 'https://cdn.shopify.com/s/files/1/0474/0910/2997/articles/5f57f31e0f6c9_1000x667.png?v=1603833538',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Memoni Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 20,
+    img: 'https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-vh1h1tn9klc5b00osm7q9re131-20180915233200.Medi.jpeg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Mughlai Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'https://www.papithaskitchen.com/wp-content/uploads/2019/03/mughlai-chicken-dum-biryani-500x375.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Chicken Roast',
+    category : 'chicken',
+    rating : 4.3,
+    price: 11,
+    img: 'https://static.toiimg.com/thumb/53007558.cms?width=1200&height=900',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Chicken Curry',
+    category : 'chicken',
+    rating : 4.3,
+    price: 10,
+    img: 'https://www.licious.in/blog/wp-content/uploads/2020/12/Chicken-Curry-recipe-600x600.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Chicken Do Pyaza',
+    category : 'chicken',
+    rating : 4.3,
+    price: 14,
+    img: 'https://allchickenrecipe.com/wp-content/uploads/2020/11/Chicken-Do-Pyaza-500x375.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Chicken Masala',
+    category : 'chicken',
+    rating : 4.3,
+    price: 12,
+    img: 'https://static.toiimg.com/thumb/84576633.cms?imgsize=418774&width=800&height=800',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Handi Chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 17,
+    img: 'https://static.tossdown.com/images/7049735575dbfa9c485bf5e48a42d1a7.jpeg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Murgh Musallam',
+    category : 'chicken',
+    rating : 4.3,
+    price: 20,
+    img: 'https://myheartbeets.com/wp-content/uploads/2019/12/instant-pot-murgh-musallam-whole-indian-chicken-500x375.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Matar Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 15,
+    img: 'https://www.indianveggiedelight.com/wp-content/uploads/2019/12/matar-paneer-instant-pot-featured.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Palak Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 10,
+    img: 'https://www.seriouseats.com/thmb/IPZNm4D6xuLuIUf-B5_WmMw9T4w=/1500x1125/filters:fill(auto,1)/20220629-PalakPaneer-Amanda-Suarez-hero-a2fdf0f3ff5141dfbf44d3977678c578.JPG',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Paneer Butter Masala',
+    category : 'paneer',
+    rating : 4.3,
+    price: 15,
+    img: 'https://www.ruchiskitchen.com/wp-content/uploads/2020/12/Paneer-butter-masala-recipe-3-500x375.jpg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Paneer Do Pyaza',
+    category : 'paneer',
+    rating : 4.3,
+    price: 12,
+    img: 'https://static.toiimg.com/thumb/59628941.cms?imgsize=364393&width=800&height=800',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Hyderabadi Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 8,
+    img: 'https://assets.limetray.com/assets/user_images/menus/compressed/1636460551_Paneertikka.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Paneer Lababdar',
+    category : 'paneer',
+    rating : 4.3,
+    price: 7,
+    img: 'https://www.indianhealthyrecipes.com/wp-content/uploads/2021/07/paneer-butter-masala.jpg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Shahi Paneer',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 5,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9iW8h1zPuEEJJnx3XltcVbpDM8tO6lq3QXQ&usqp=CAU',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Navratan Korma',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 8,
+    img: 'https://www.archanaskitchen.com/images/archanaskitchen/1-Author/Preeti_Tamilarasan/Navratan_Korma.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Veg Jalfrezi',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 7,
+    img: 'https://2.bp.blogspot.com/-QnqYcRMh4GI/T0lSAodMhCI/AAAAAAAAAiU/rxNVld3t1VQ/s1600/IMG_8079-2.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Veg Biryani',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 5,
+    img: 'https://i.ytimg.com/vi/JqP4wSWsYr4/maxresdefault.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Veg Curry',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 7,
+    img: 'https://i0.wp.com/cheerfulchoices.com/wp-content/uploads/2020/10/Vegetable-Curry-4-scaled.jpeg?resize=720%2C720&ssl=1',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Veg Kolhapur',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 10,
+    img: 'https://tastedrecipes.com/wp-content/uploads/2022/01/Veg-Kolhapuri-19.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Veg Masala',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 4,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYg1egFEAmR_VnIFr3p1DdllUgt2B3bJai_w&usqp=CAU',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Veg Pakora',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 4,
+    img: 'https://i.ytimg.com/vi/_c7WcO7Lwz0/maxresdefault.jpg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Momos',
+    category : 'chinese',
+    rating : 4.3,
+    price: 8,
+    img: 'https://www.tiffinboxfood.com/wp-content/uploads/2020/03/chicken-momo-1.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Chicken Manchurian',
+    category : 'chinese',
+    rating : 4.3,
+    price: 7,
+    img: 'https://www.chilitochoc.com/wp-content/uploads/2020/06/ChickenManchurian1-e1596062497253.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Chili Chicken',
+    category : 'chinese',
+    rating : 4.3,
+    price: 5,
+    img: 'https://static.toiimg.com/thumb/53094926.cms?width=1200&height=900',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Chowmein',
+    category : 'chinese',
+    rating : 4.3,
+    price: 16,
+    img: 'https://www.chilitochoc.com/wp-content/uploads/2021/03/Desi-Chow-Mein-featured.jpg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Spring Roll',
+    category : 'chinese',
+    rating : 4.3,
+    price: 14,
+    img: 'https://www.cubesnjuliennes.com/wp-content/uploads/2021/01/Spring-Roll-Recipe.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Szechuan Chicken',
+    category : 'chinese',
+    rating : 4.3,
+    price: 10,
+    img: 'https://myfoodstory.com/wp-content/uploads/2022/02/Szechuan-Chicken-3-500x375.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Fried Rice',
+    category : 'chinese',
+    rating : 4.3,
+    price: 8,
+    img: 'https://www.indianhealthyrecipes.com/wp-content/uploads/2020/12/fried-rice.jpg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Butter Masala Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 18,
+    img: 'https://www.nandanmsd.com/wp-content/uploads/2020/08/Butter-Masala-Dosa.png',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Idli',
+    category : 'south indian',
+    rating : 4.3,
+    price: 20,
+    img: 'https://spicytamarind.com.au/wp-content/uploads/2018/07/idli-fi.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Masala Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 12,
+    img: 'https://static.toiimg.com/thumb/75598971.cms?imgsize=1977194&width=800&height=800',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Mysore Bonda',
+    category : 'south indian',
+    rating : 4.3,
+    price: 10,
+    img: 'https://www.awesomecuisine.com/wp-content/uploads/2015/08/mangalore-bonda.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Onion Uttapam',
+    category : 'south indian',
+    rating : 4.3,
+    price: 15,
+    img: 'https://sanjeevkapoor.com/UploadFiles/RecipeImages/Onion-Uttapam---SK-Khazan.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Plain Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 40,
+    img: 'http://sherebengal.com/wp-content/uploads/2017/09/Butter-plain-dosa.jpg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Rava Uttapam',
+    category : 'south indian',
+    rating : 4.3,
+    price: 25,
+    img: 'https://static.toiimg.com/thumb/60852168.cms?imgsize=132396&width=800&height=800',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Sambhar Vada',
+    category : 'south indian',
+    rating : 4.3,
+    price: 34,
+    img: 'https://foodiewish.com/wp-content/uploads/2020/05/Medu-Vada-Recipe-1.jpg',
+    quantity: 1
+},
+]
+
+export {foodItem};
